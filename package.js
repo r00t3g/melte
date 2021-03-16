d403362615775f6312d2add397720471ca95567f
r00t3g <daven@wisebits.com>
2021-03-16 12:08:45 +0200

    Package preparation

--- package.js
+++ package.js
@@ -30,8 +30,8 @@ Package.registerBuildPlugin({
     'periscopic': '2.0.2',
     'svelte-hmr': hmrVersion,
     'acorn': '7.4.0',
-    'svelte-preprocess': '4.6.9',
     'typescript': '4.1.2',
+    'node-sass': '5.0.0',
   }
 });
 
